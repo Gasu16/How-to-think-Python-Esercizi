4ff4873d55b7141637c3f02b24d286733a3c0524
Matteo Grammatico <kizaru774@gmail.com>
2017-09-30 16:26:09 +0200

    Add files via upload

---
+++ PyGame/primoGame.py
@@ -0,0 +1,30 @@
+# Pixel random on the screen
+
+import pygame
+from pygame.locals import *
+import random
+
+pygame.init()
+
+width = 640
+height = 480
+
+screen = pygame.display.set_mode((width, height))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    x = random.randint(0, width-1)
+    y = random.randint(0, height-1)
+    red = random.randint(0, 255)
+    green = random.randint(0, 255)
+    blue = random.randint(0, 255)
+
+    screen.set_at((x, y), (red, green, blue))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+    clock.tick(240)
