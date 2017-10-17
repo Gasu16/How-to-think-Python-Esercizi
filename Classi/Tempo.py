e2a5ab9b9b91b329b39564248b4012b8b48f3ace
Matteo Grammatico <kizaru774@gmail.com>
2017-10-17 17:54:55 +0200

    Add files via upload

--- Classi/Tempo.py
+++ Classi/Tempo.py
@@ -8,9 +8,12 @@ def setTempo(ore, minuti, secondi):
     Tempo.minuti = minuti
     Tempo.secondi = secondi
 
-
 def stampaTempo(Tempo):
     return "" + str(Tempo.ore) + ":" + str(Tempo.minuti) + ":" + str(Tempo.secondi)
+
+def sommaTempo(T1, T2):
+    Secondi = ConverteInSecondi(T1) + ConverteInSecondi(T2)
+    return ConverteInTempo(Secondi)
 
 def Dopo(T1, T2):
     if T1 >= T2:
@@ -18,10 +21,43 @@ def Dopo(T1, T2):
     else:
         return False
 
+def Incremento(Tempo, Secondi):
+    Tempo.secondi += Secondi
+    while Tempo.secondi >= 60:
+        Tempo.secondi -= 60
+        Tempo.minuti += 1
+    while Tempo.minuti >= 60:
+        Tempo.minuti -= 60
+        Tempo.ore += 1
+
+def newIncremento(Tempo, Secondi):
+    while ConverteInSecondi(Tempo) >= 60:
+        return ConverteInSecondi(Tempo)
+
+#### CONVERTE UN OGGETTO TEMPO IN SECONDI ####
+def ConverteInSecondi(Tempo):
+    minuti = Tempo.ore * 60 + Tempo.minuti
+    secondi = Tempo.minuti * 60 + Tempo.secondi
+    return secondi
+
+#### CONVERTE I SECONDI IN UN OGGETTO TEMPO ####
+def ConverteInTempo(Secondi):
+    T = Tempo()
+    T.ore = Secondi/3600
+    Secondi = Secondi - T.ore * 3600
+    T.minuti = Secondi/60
+    Secondi = Secondi - T.minuti * 60
+    T.secondi = Secondi
+    return T
+
 def main(args):
     Time = Tempo()
+    tempo1 = Tempo()
+    tempo2 = Tempo()
     setTempo(23, 35, 40)
     print stampaTempo(Time)
+    print stampaTempo(sommaTempo(tempo1, tempo2))
+    print newIncremento(Time, 10)
     return 0
 
 if __name__ == '__main__':
