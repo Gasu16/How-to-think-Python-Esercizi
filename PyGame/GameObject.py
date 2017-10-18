a2c9dd0961e865ee6258362327a78e3442cc5265
Matteo Grammatico <kizaru774@gmail.com>
2017-10-18 19:15:41 +0200

    Add files via upload

---
+++ PyGame/GameObject.py
@@ -0,0 +1,42 @@
+import time
+import datetime
+import pygame
+from pygame.locals import *
+import random
+
+class GameObject:
+    def __init__(self, image, height, speed):
+        self.speed = speed
+        self.image = image
+        self.pos = image.get_rect().move(0, height)
+
+    def move(self):
+        self.pos = self.pos.move(0, self.speed)
+        if self.pos.right >= 600:
+            self.pos.left = 0
+
+def main(args):
+    screen = pygame.display.set_mode((1280, 640))
+    player = pygame.image.load("player.bmp").convert()
+    background = pygame.image.load("background.bmp").convert()
+    screen.blit(background, (0, 0))
+    objects = []
+    for x in range(1):
+        o = GameObject(player, x*40, x)
+        objects.append(o)
+    while 1:
+        for event in pygame.event.get():
+            if event.type in (QUIT, KEYDOWN):
+                sys.exit()
+        for o in objects:
+            screen.blit(background, o.pos, o.pos)
+        for o in objects:
+            o.move()
+            screen.blit(o.image, o.pos)
+        pygame.display.update()
+        pygame.time.delay(100)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
